5b4e880b3167f801c79a360610641b5cf300d170
hasouza <hasouza@dgae.mec.pt>
2019-02-07 13:02:53 +0000

    Primeiro commit

---
+++ main-oo.py
@@ -0,0 +1,26 @@
+from veiculo import Veiculo
+from carro import Carro
+
+caminhao_rosa = Veiculo('rosa', 6, 'Ford', 10)
+print('')
+print('Caminhão Rosa')
+print(caminhao_rosa)
+print(type(caminhao_rosa))
+print("Cor:", caminhao_rosa.cor)
+print("Marca:", caminhao_rosa.marca)
+print("Tanque:", caminhao_rosa.tanque)
+
+carro_azul = Carro('azul', 'BMW', 30)
+print('')
+print('Carro Azul')
+print(carro_azul)
+print(type(carro_azul))
+print("Cor:", carro_azul.cor)
+print("Marca:", carro_azul.marca)
+print("Tanque:", carro_azul.tanque)
+carro_azul.abastecer(25)
+print("Tanque:", carro_azul.tanque)
+carro_azul.abastecer(20)
+print("Tanque:", carro_azul.tanque)
+carro_azul.abastecer(10)
+print("Tanque:", carro_azul.tanque)
